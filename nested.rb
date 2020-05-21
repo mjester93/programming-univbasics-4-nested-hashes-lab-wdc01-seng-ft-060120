d60f8decadeb6efcfab631685cbf77598bf4e503
Michael Jester <m.jester.93@gmail.com>
2020-05-21 23:28:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -97,6 +97,8 @@ def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable.
   # return the entire updated hash
 
+	alans_new_info = "Making Windows better than Mac"
+
 	programmer_hash =
  		{
       :grace_hopper => {
@@ -113,6 +115,7 @@ def changing_alan
       }
     }
 
+		return programmer_hash
 end
 
 def adding_to_dennis
